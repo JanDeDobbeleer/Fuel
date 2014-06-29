5562fc2af89f4d6720ab8dcaaf4bcda92f96f34a
Jan Joris <jan.dedobbeleer@twipemobile.com>
2014-06-29 21:10:55 +0200

    - added correct file (did not save at first)

--- Test.Oauth/Test/OauthTest.cs
+++ Test.Oauth/Test/OauthTest.cs
@@ -15,7 +15,7 @@ namespace Test.Oauth.Test
             OauthToken token;
             using (var xauth = new XAuth())
             {
-                token = await xauth.XAuthAccessTokenRequest("test", "test", Credentials.Credentials.GetInstance(), "https://mobilevikings.com:443/api/2.0/oauth/access_token/");
+                token = await xauth.XAuthAccessTokenRequest("test", "test", Credentials.GetInstance(), "https://mobilevikings.com:443/api/2.0/oauth/access_token/");
             }
             Assert.AreNotEqual(string.Empty, token.TokenKey);
             Assert.AreNotEqual(string.Empty, token.TokenSecret);
@@ -27,13 +27,13 @@ namespace Test.Oauth.Test
             OauthToken token;
             using (var xauth = new XAuth())
             {
-                token = await xauth.XAuthAccessTokenRequest("test", "test", Credentials.Credentials.GetInstance(), "https://mobilevikings.com:443/api/2.0/oauth/access_token/");
+                token = await xauth.XAuthAccessTokenRequest("test", "test", Credentials.GetInstance(), "https://mobilevikings.com:443/api/2.0/oauth/access_token/");
             }
             string response;
             using (var oauth = new Fuel.Oauth.Utils.Oauth())
             {
                 var parameters = new List<QueryParameter> { new QueryParameter("alias", "1") };
-                response = await oauth.OAuthRequest(Credentials.Credentials.GetInstance(), token, "https://mobilevikings.com:443/api/2.0/oauth/msisdn_list.json", parameters);
+                response = await oauth.OAuthRequest(Credentials.GetInstance(), token, "https://mobilevikings.com:443/api/2.0/oauth/msisdn_list.json", parameters);
             }
             Assert.AreNotEqual(string.Empty, response);
         }
